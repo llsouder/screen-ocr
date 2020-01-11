1989e3126b9495d6bc6e236bf566b597063d887b
lonnie <lonnie@souderfamily.net>
2020-01-11 10:46:03 -0500

    Most code I want to copy/paste uses cv2.

--- hsv.py
+++ hsv.py
@@ -1,13 +1,13 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 def callback(x):
     pass
 
-cap = cv.VideoCapture("jpvd.mkv")
+cap = cv2.VideoCapture("jpvd.mkv")
 print("testing...")
 
-cv.namedWindow('Colorbars')
+cv2.namedWindow('Colorbars')
  
 #assign strings for ease of coding
 hh='Hue High'
@@ -18,12 +18,12 @@ vh='Value High'
 vl='Value Low'
 wnd = 'Colorbars'
 #Begin Creating trackbars for each
-cv.createTrackbar(hl, wnd,44,179,callback)
-cv.createTrackbar(hh, wnd,70,179,callback)
-cv.createTrackbar(sl, wnd,95,255,callback)
-cv.createTrackbar(sh, wnd,172,255,callback)
-cv.createTrackbar(vl, wnd,158,255,callback)
-cv.createTrackbar(vh, wnd,255,255,callback)
+cv2.createTrackbar(hl, wnd,44,179,callback)
+cv2.createTrackbar(hh, wnd,70,179,callback)
+cv2.createTrackbar(sl, wnd,95,255,callback)
+cv2.createTrackbar(sh, wnd,172,255,callback)
+cv2.createTrackbar(vl, wnd,158,255,callback)
+cv2.createTrackbar(vh, wnd,255,255,callback)
 
 
 
@@ -32,16 +32,16 @@ _, frame_orig = cap.read()
 while(1):
     frame = frame_orig.copy()
     # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # define range of blue color in HSV
 
 
-    hul=cv.getTrackbarPos(hl, wnd)
-    huh=cv.getTrackbarPos(hh, wnd)
-    sal=cv.getTrackbarPos(sl, wnd)
-    sah=cv.getTrackbarPos(sh, wnd)
-    val=cv.getTrackbarPos(vl, wnd)
-    vah=cv.getTrackbarPos(vh, wnd)
+    hul=cv2.getTrackbarPos(hl, wnd)
+    huh=cv2.getTrackbarPos(hh, wnd)
+    sal=cv2.getTrackbarPos(sl, wnd)
+    sah=cv2.getTrackbarPos(sh, wnd)
+    val=cv2.getTrackbarPos(vl, wnd)
+    vah=cv2.getTrackbarPos(vh, wnd)
  
     #make array for final values
     HSVLOW=np.array([hul,sal,val])
@@ -50,23 +50,23 @@ while(1):
     #lower_blue = np.array([50,55,55])
     #upper_blue = np.array([60,255,255])
     # Threshold the HSV image to get only blue colors
-    mask = cv.inRange (hsv, HSVLOW, HSVHIGH)
+    mask = cv2.inRange (hsv, HSVLOW, HSVHIGH)
 
-    bluecnts = cv.findContours(mask.copy(),
-                              cv.RETR_EXTERNAL,
-                              cv.CHAIN_APPROX_SIMPLE)[-2]
+    bluecnts = cv2.findContours(mask.copy(),
+                              cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)[-2]
 
     for i in range(0, len(bluecnts)):
-        (xg,yg,wg,hg) = cv.boundingRect(bluecnts[i])
+        (xg,yg,wg,hg) = cv2.boundingRect(bluecnts[i])
         if hg > 1:
-            cv.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(255,0,9),2)
+            cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(255,0,9),2)
 
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    cv.imshow('frame',frame)
-    #cv.imshow('mask',mask)
-    #cv.imshow('res',res)
-    k = cv.waitKey(5) & 0xFF
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    cv2.imshow('frame',frame)
+    #cv2.imshow('mask',mask)
+    #cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
